baac7503bf33f4b03b683461767d7dfa6c20b231
= <=>
2024-11-20 08:45:49 +0530

    Checkpoint commit

---
+++ backend/auth_service/Readme.md
@@ -0,0 +1,18 @@
+# Authentication Service
+
+Clone the folder to run the server
+
+## Build
+
+Ensure node and npm are installed on the system
+
+```bash
+npm i
+tsc -p .
+
+```
+
+## Run Server
+```bash
+node dist/index.js
+```
--- backend/auth_service/src/app.ts
+++ backend/auth_service/src/app.ts
@@ -1,5 +1,13 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 
 const app = express();
+
+app.get("/", (_:Request, res: Response, next: NextFunction) => {
+    res.json({
+        response: "Hello, Vatar"
+    })
+
+    next()
+})
 
 export default app;
